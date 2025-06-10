179f686157df2553e875add5e0f38bdd7da3b4d6
tomo082 <131239927+tomo082@users.noreply.github.com>
2025-06-10 20:13:28 +0900

    Update main.py
    add mvtec_to_mvtec

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ FIRST_STAGE_EPOCH = 10
 SETTINGS = {'visa_to_mvtec': VISA_TO_MVTEC, 'mvtec_to_visa': MVTEC_TO_VISA,
             'mvtec_to_btad': MVTEC_TO_BTAD, 'mvtec_to_mvtec3d': MVTEC_TO_MVTEC3D,
             'mvtec_to_mpdd': MVTEC_TO_MPDD, 'mvtec_to_mvtecloco': MVTEC_TO_MVTECLOCO,
-            'mvtec_to_brats': MVTEC_TO_BRATS}
+            'mvtec_to_brats': MVTEC_TO_BRATS,'mvtec_to_mvtec': MVTEC_TO_MVTEC}
 
 
 def main(args):
