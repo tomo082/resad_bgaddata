41c6cbc507baef814b1cf5272650baa0549ebdeb
Tomoya Ueno <tomoyaueno0829@gmail.com>
2025-06-28 01:55:09 +0900

    6/27ゼミ

--- classes.py
+++ classes.py
@@ -49,4 +49,3 @@ MVTECFEW_TO_MVTEC = {'seen': ['capsule','screw','transistor'],
                   'unseen': ['bottle', 'cable', 'capsule', 'carpet', 'grid',
                              'hazelnut', 'leather', 'metal_nut', 'pill', 'screw',
                              'tile', 'toothbrush', 'transistor', 'wood', 'zipper']}
-
